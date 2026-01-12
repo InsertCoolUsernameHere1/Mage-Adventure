68f3770e6eb7e62adb5a35e5db836319dc945712
InsertCoolUsernameHere1 <benjamin.volante.153@accesscreative.ac.uk>
2026-01-12 11:52:31 +0000

    Player can take damage and can die + death screen

--- White box Start Map/Assets/Death screen.cs
+++ White box Start Map/Assets/Death screen.cs
@@ -1,10 +1,9 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-public class MainMenu : MonoBehaviour
+public class Deathscreen : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-     public void startButton()
+    public void retryButton()
     {
         SceneManager.LoadScene("Tutorial");
     }
--- White box Start Map/Assets/Scripts/Attacks/Player health.cs
+++ White box Start Map/Assets/Scripts/Attacks/Player health.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Playerhealth : MonoBehaviour
 {
@@ -11,26 +12,27 @@ public class Playerhealth : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        maxHealth = health;
     }
 
     // Update is called once per frame
     void Update()
     {
         healthText.text = health + " / " + maxHealth;
-        healthBar.value = (float)health / (float)maxHealth;
+        healthBar.value = (float)health / (float)maxHealth; // Creates a percentage for the healthabr to use to see how much the bar should be filled
         if (health <= 0)
         {
             healthBar.gameObject.SetActive(false);
             Destroy(gameObject);
+            SceneManager.LoadScene("Death screen"); // Loads up the death screen
         }
     }
-    private void OnCollisionEnter(Collision other)
+    private void OnTriggerEnter(Collider other)
     {
         {
             if (other.gameObject.tag == "Sword")
             {
-                health = health - 2;
+                health = health - 2;  // Detects if an item with the tag "Sword is touching the players hitbox
             }
         }
     }
